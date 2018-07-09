fb8c3ca4a1b549a500b3de12c1cee833a60ad60c
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2018-07-09 15:08:01 +0200

    refactor: refactor sqlserver testcontainers integration test

--- src/test/kotlin/org/vld/template/ApplicationTestIT.kt
+++ src/test/kotlin/org/vld/template/ApplicationTestIT.kt
@@ -2,7 +2,6 @@ package org.vld.template
 
 import org.junit.Before
 import org.junit.BeforeClass
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.slf4j.Logger
@@ -12,15 +11,7 @@ import org.springframework.boot.jdbc.DataSourceBuilder
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.test.context.junit4.SpringRunner
-import org.testcontainers.containers.BindMode
-import org.testcontainers.containers.GenericContainer
-import org.testcontainers.containers.output.Slf4jLogConsumer
-import org.testcontainers.jdbc.ContainerDatabaseDriver
 import javax.sql.DataSource
-
-//class KPostgreSQLContainer(imageName: String) : PostgreSQLContainer<KPostgreSQLContainer>(imageName)
-
-//class KGenericContainer(imageName: String) : GenericContainer<KGenericContainer>(imageName)
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
@@ -29,15 +20,12 @@ class ApplicationTestIT {
     @Autowired
     private lateinit var dataSource: DataSource
 
-    // ** SQL Server
-
-    // Testcontainers
     /*@Before
     fun setupDataSourceForEachTest() {
         dataSource = DataSourceBuilder.create()
-                .url("jdbc:tc:mssqlserver:2017-GA://localhost:1433;databaseName=master")
+                .url("jdbc:tc:mssqlserver:2017-GA://localhost:1433;databaseName=people?TC_INITSCRIPT=init/sqlserver_init.sql")
                 .username("sa")
-                .password("Password!1")
+                .password("Password1!")
                 .driverClassName("org.testcontainers.jdbc.ContainerDatabaseDriver")
                 .build()
     }*/
@@ -48,16 +36,13 @@ class ApplicationTestIT {
 
 //        private lateinit var dataSource: DataSource
 
-        // ** SQL Server
-
-        // Testcontainers
         /*@BeforeClass
         @JvmStatic
         fun setupDataSourceForAllTests() {
             dataSource = DataSourceBuilder.create()
-                    .url("jdbc:tc:mssqlserver:2017-GA://localhost:1433;databaseName=master")
+                    .url("jdbc:tc:mssqlserver:2017-GA://localhost:1433;databaseName=people?TC_INITSCRIPT=init/sqlserver_init.sql")
                     .username("sa")
-                    .password("Password!1")
+                    .password("Password1!")
                     .driverClassName("org.testcontainers.jdbc.ContainerDatabaseDriver")
                     .build()
         }*/
